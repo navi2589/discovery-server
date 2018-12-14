b15a5d7ff42214517ecd262eb4cc57a3eaa98a1c
Navnath Reshmaji Chinchore <nchinchore@athenahealth.com>
2018-12-14 18:50:37 +0530

    DS-1 : initail commit

---
+++ src/test/java/com/expenceguru/gateway/expencegurugateway/ExpencegurugatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.expenceguru.gateway.expencegurugateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpencegurugatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
